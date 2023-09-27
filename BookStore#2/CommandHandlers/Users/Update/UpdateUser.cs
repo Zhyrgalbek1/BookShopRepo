cdb73ebd3af9e9a45c08d00d64927474b801461d
Zhyrgalbek1 <jyrgalzarylbekov@gmail.com>
2023-09-27 23:51:30 +0600

    FinishBetaProject

--- BookStore#2/CommandHandlers/Users/Update/UpdateUser.cs
+++ BookStore#2/CommandHandlers/Users/Update/UpdateUser.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace CommandHandlers.Usesrs.Update;
 
-public record UpdateUserResponse
+public record UpdateUser
 {
     public long Id { get; init; }
     public required string Username { get; init; }
@@ -13,7 +13,7 @@ public record UpdateUserResponse
     public required string LastName { get; set; }
     public required string Email { get; set; }
 }
-public record UpdateUserCommand : IRequest<UpdateUserResponse?>
+public record UpdateUserCommand : IRequest<UpdateUser?>
 {
     public required string Username { get; init; }
     public required string Password { get; init; }
--- BookStore#2/CommandHandlers/Users/Update/UpdateUserHandler.cs
+++ BookStore#2/CommandHandlers/Users/Update/UpdateUserHandler.cs
@@ -2,10 +2,12 @@
 using CommandHandlers.Usesrs.Update;
 using Domain.Repos;
 using MediatR;
+using Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CommandHandlers.Users.Update;
 
-internal class UpdateUserHandler : IRequestHandler<UpdateUserCommand, UpdateUserResponse?>
+internal class UpdateUserHandler : IRequestHandler<UpdateUserCommand, UpdateUser?>
 {
     private readonly IUserRepo _userRepository;
     private readonly IUnitOfWork _unitOfWork;
@@ -16,7 +18,7 @@ internal class UpdateUserHandler : IRequestHandler<UpdateUserCommand, UpdateUser
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<UpdateUserResponse?> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    public async Task<UpdateUser?> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
         var user = await _userRepository.GetByUsernameAsync(request.Username, cancellationToken);
         if (user is not null)
@@ -24,11 +26,17 @@ internal class UpdateUserHandler : IRequestHandler<UpdateUserCommand, UpdateUser
             user.Username ??= request.Username;
             user.PasswordHash ??= request.Password;
             user.Role = request.Role;
+            user.Profile = new UserProfile
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+            };
 
             _userRepository.Update(user);
             await _unitOfWork.CommitAsync();
 
-            var response = new UpdateUserResponse
+            var response = new UpdateUser
             {
                 Id = user.Id,
                 Username = user.Username,
